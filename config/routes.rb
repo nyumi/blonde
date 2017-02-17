17702872261c73b4664cf4ee73a0b2049de5a6ae
nyum <yumt8786@gmail.com>
2017-02-17 18:34:19 +0900

    ログインするまで🐷🐷

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+
+  get 'pages/show'
+
   devise_for :users
-  get 'welcome/index'
-  root to: "top#index"
+  # root to: "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
