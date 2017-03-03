150e3bc160a1bc7e424ceb897b41523ab0c72eca
nyum <yumt8786@gmail.com>
2017-03-03 17:34:07 +0900

    "#11 create migration for add user_id for watch_list_views 🐷"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226105757) do
+ActiveRecord::Schema.define(version: 20170303080021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
